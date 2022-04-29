7f32e9dcaf6d30356a9c72d556a53fc5c1712371
Jeferson Rodriguez <jeferson_88@hotmail.com>
2022-04-29 02:56:55 -0500

    Update 03_parametrizacion_variedades.R

--- 03_parametrizacion_variedades.R
+++ 03_parametrizacion_variedades.R
@@ -5,6 +5,7 @@
 
 
 library(data.table)
+library(scales)
 source("https://raw.githubusercontent.com/jrodriguez88/agroclimR/r_package/R_package/run_tools/extract_drates_param.R", encoding = "UTF-8")
 source("https://raw.githubusercontent.com/jrodriguez88/agroclimR/r_package/R_package/run_tools/run_drates_param.R", encoding = "UTF-8")
 
@@ -21,21 +22,65 @@ eval_set <- setdiff(exp_files, cal_set)
 path_cal = 'data/OUTPUTS/'
 download_ORYZA_Tools(path_cal)
 run_drates_param(cal_set, path_cal)
-extract_drates_param(cal_set, path_cal)
+
+### Extract and import params from PARAM.out
+raw_params <- extract_drates_param(cal_set, path_cal)
+tidy_params <- tidy_params_oryza(params, 2)
 
 
 
+###############################################################
+### PARAM PLOTS
+
+###  Development Rates 
+DVR_plot1(tidy_params$DVR_tb, save_plot = "N")  #point
 
 
-## Plots for Development Rates 
-DVR_plot1(DVR_df, save_plot = "N")  #point
-DVR_plot2(DVR_df, save_plot = "N")  #density
-DVR_plot3(DVR_df, save_plot = "N")  #stat summary
-
-# Plots for LAI, SLA and CC
-## Plots for Biomass Partition ('PF_m1': Oryza_manual,
-#                               'PF_m2: Samurai)
-
-BPART_plot1(PF_m2, save_plot = "N") #all data
-BPART_plot2(PF_m1, save_plot = "N") #facet by site & pf
+##  Biomass Partition 
+BPART_plot1(tidy_params$PF_tb, save_plot = "N") #all data
 BPART_plot3(PF_m1, save_plot = "N") #facet by pf
+
+##  Fraction of carbohydrates allocated to stems that is stored as reserves (FSTR)
+FSTR_plot(raw_params$FSTR_df, save_plot = "Y")  # by site
+
+##  Leaf death coefficient
+DRLV_plot (raw_params$DRLV_df, llim = 1.1, save_plot = "Y")# by 
+
+
+## Spikelet growth factor
+SPGF_df <- map(data$data, SPGF_extract) %>% bind_rows
+SPGF_plot(SPGF_df, save_plot = "N")
+
+##################################################
+
+## DVS is a numeric vector with lenght>3, Contain the proposed Development stage
+#  DVS = 0.00 0.25 0.50 0.75 1.00 1.50 2.00 2.50
+DVS <- c(seq(0, 0.8, 0.25), seq(1, 2.5, 0.5))
+span = 0.75 ; nse = 4
+
+## Compute mean, max and minimun value. Require set span and nse arguments, 
+PF_tb <- loess_crp_oryza(tidy_params$PF_tb, DVS, span, nse)
+plot_pf_loess(tidy_params$PF_tb, pf_tbs, span, nse)
+
+DVS[2] <- 0.35
+SLA_tb <- loess_crp_oryza(tidy_params$SLA_tb, DVS, span = 0.75, nse = 2)
+plot_sla_loess(tidy_params$SLA_tb, SLA_tb, span = 0.75, 0.5, 2)
+
+
+SPGF <- SPGF_cal(SPGF_df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
